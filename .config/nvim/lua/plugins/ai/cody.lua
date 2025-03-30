b2d9e3cd1beb1719c6503e23c9536efeabdf1e5c
Evhen Malysh <malysh.evgeniy@gmail.com>
2025-03-30 20:01:34 +0300

    update cspell

--- .config/nvim/lua/plugins/ai/cody.lua
+++ .config/nvim/lua/plugins/ai/cody.lua
@@ -1,22 +1,25 @@
 local M = { 'sourcegraph/sg.nvim' }
+
 M.lazy = false
+
 M.dependencies = { 'nvim-lua/plenary.nvim' }
+
 M.config = function()
   local sg = require 'sg'
   sg.setup {
-    -- definition = 'gd',
-    -- references = 'gr',
-    -- hover = 'K',
-    -- rename = 'gR',
-    -- implementation = 'gi',
-    -- typeDefinition = 'gT',
+    definition = 'gd',
+    references = 'gr',
+    hover = 'K',
+    rename = 'gR',
+    implementation = 'gi',
+    typeDefinition = 'gT',
   }
 end
 
 -- stylua: ignore
 M.keys = {
-        { '<leader>cc', '<cmd>CodyChat<cr>',    desc = 'Cody Chat' },
-        { '<leader>ce', '<cmd>CodyExplain<cr>', desc = 'Cody Chat' },
+  { '<leader>cc', '<cmd>CodyChat<cr>',    desc = 'Cody Chat' },
+  { '<leader>ce', '<cmd>CodyExplain<cr>', desc = 'Cody Chat' },
 }
 
 return M
