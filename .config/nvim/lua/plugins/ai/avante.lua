4397bdc2a364fe0a69da97d2fe7fe6b347457c99
Evhen Malysh <malysh.evgeniy@gmail.com>
2025-04-02 20:56:52 +0300

    Update Neovim plugins and add Avante.nvim configuration
    - Add new plugins: avante.nvim, copilot.lua, dressing.nvim, fzf-lua, img-clip.nvim, mini.pick, nvim-web-devicons
    - Update existing plugin versions in lazy-lock.json
    - Create new configuration file for Avante.nvim (ai/avante.lua)
    - Set up Avante.nvim with dependencies, options, and custom initialization
    - Add ranger colorschemes as a submodule

---
+++ .config/nvim/lua/plugins/ai/avante.lua
@@ -0,0 +1,64 @@
+local M = { 'yetone/avante.nvim' }
+
+M.dependencies = {
+  'nvim-treesitter/nvim-treesitter',
+  'stevearc/dressing.nvim',
+  'nvim-lua/plenary.nvim',
+  'MunifTanjim/nui.nvim',
+  --- The below dependencies are optional,
+  'ibhagwan/fzf-lua', -- for file_selector provider fzf
+  'echasnovski/mini.icons',
+  'zbirenbaum/copilot.lua', -- for providers='copilot'
+  {
+    -- support for image pasting
+    'HakonHarnes/img-clip.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- recommended settings
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        -- required for Windows users
+        use_absolute_path = true,
+      },
+    },
+  },
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { 'markdown', 'Avante' },
+    },
+    ft = { 'markdown', 'Avante' },
+  },
+}
+
+M.event = 'VeryLazy'
+
+M.version = false -- Never set this value to "*"! Never!
+
+M.opts = {
+  -- add any opts here
+  -- for example
+  provider = 'gemini',
+}
+-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+M.build = 'make'
+
+-- Custom hack to force gemini to load instead of claude
+-- 2024-09-13
+M.init = function()
+  vim.api.nvim_create_autocmd('VimEnter', {
+    callback = function()
+      -- Ensure Avante.nvim has been loaded before switching providers
+      if vim.fn.exists ':AvanteSwitchProvider' == 2 then
+        vim.cmd 'AvanteSwitchProvider gemini'
+      end
+    end,
+  })
+end
+
+return M
