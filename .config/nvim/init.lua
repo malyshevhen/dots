8aec64c4b18b69bc1d0c6953988f334be8474224
Evhen Malysh <malysh.evgeniy@gmail.com>
2025-09-08 16:58:13 +0300

    feat: lsp reference highlight

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -110,9 +110,6 @@ vim.diagnostic.config {
 --- Dashboard
 require('alpha').setup(require('alpha.themes.dashboard').config)
 
---- Autopairs
-require('nvim-autopairs').setup { map_cr = true }
-
 --- Session Management
 require('auto-session').setup {
   suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/', '/tmp', '/private/tmp' },
@@ -248,20 +245,27 @@ require('luasnip').config.setup {
 }
 
 --- Mini.nvim
-local mini_modules = {
-  'pairs',
-  'surround',
-  'ai',
-  'move',
-  'operators',
-  'comment',
-  'jump',
-  'icons',
-}
+require('mini.pairs').setup()
+require('mini.move').setup {
+  mappings = {
+    left = '<M-h>',
+    right = '<M-l>',
+    down = '<M-j>',
+    up = '<M-k>',
 
-for _, module in ipairs(mini_modules) do
-  require('mini.' .. module).setup()
-end
+    line_left = '<M-h>',
+    line_right = '<M-l>',
+    line_down = '<M-j>',
+    line_up = '<M-k>',
+  },
+
+  options = {
+    reindent_linewise = true,
+  },
+}
+require('mini.operators').setup()
+require('mini.comment').setup()
+-- require('mini.icons').setup()
 
 --- Testing
 require('neotest').setup {
@@ -548,6 +552,7 @@ require('snacks').setup {
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
   callback = function()
+    local Snacks = Snacks
     -- Setup debugging globals
     _G.dd = function(...)
       Snacks.debug.inspect(...)
@@ -650,6 +655,67 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+--- Illuminate
+require('illuminate').configure {
+  -- providers: provider used to get references in the buffer, ordered by priority
+  providers = {
+    'lsp',
+    'treesitter',
+    'regex',
+  },
+  -- delay: delay in milliseconds
+  delay = 100,
+  -- filetype_overrides: filetype specific overrides.
+  -- The keys are strings to represent the filetype while the values are tables that
+  -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+  filetype_overrides = {},
+  -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+  filetypes_denylist = {
+    'dirbuf',
+    'dirvish',
+    'fugitive',
+  },
+  -- filetypes_allowlist: filetypes to illuminate, this is overridden by filetypes_denylist
+  -- You must set filetypes_denylist = {} to override the defaults to allow filetypes_allowlist to take effect
+  filetypes_allowlist = {},
+  -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+  -- See `:help mode()` for possible values
+  modes_denylist = {},
+  -- modes_allowlist: modes to illuminate, this is overridden by modes_denylist
+  -- See `:help mode()` for possible values
+  modes_allowlist = {},
+  -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+  -- Only applies to the 'regex' provider
+  -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+  providers_regex_syntax_denylist = {},
+  -- providers_regex_syntax_allowlist: syntax to illuminate, this is overridden by providers_regex_syntax_denylist
+  -- Only applies to the 'regex' provider
+  -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+  providers_regex_syntax_allowlist = {},
+  -- under_cursor: whether or not to illuminate under the cursor
+  under_cursor = true,
+  -- large_file_cutoff: number of lines at which to use large_file_config
+  -- The `under_cursor` option is disabled when this cutoff is hit
+  large_file_cutoff = 10000,
+  -- large_file_config: config to use for large files (based on large_file_cutoff).
+  -- Supports the same keys passed to .configure
+  -- If nil, vim-illuminate will be disabled for large files.
+  large_file_overrides = nil,
+  -- min_count_to_highlight: minimum number of matches required to perform highlighting
+  min_count_to_highlight = 1,
+  -- should_enable: a callback that overrides all other settings to
+  -- enable/disable illumination. This will be called a lot so don't do
+  -- anything expensive in it.
+  should_enable = function(bufnr)
+    return true
+  end,
+  -- case_insensitive_regex: sets regex case sensitivity
+  case_insensitive_regex = false,
+  -- disable_keymaps: disable default keymaps
+  disable_keymaps = false,
+}
+
+local Snacks = Snacks
 
 --------------------------------------------------------------------------------
 -------------------------------- KEYBINDINGS -----------------------------------
@@ -658,7 +724,7 @@ vim.o.foldenable = true
 -- stylua: ignore
 K:map {
   -- System clipboard operations
-  { map = '<leader>y',       cmd = '"+y',                                                                                     desc = 'Yank (Clipboard)',                mode = { 'v', 'x' }, },
+  { map = '<leader>y',       cmd = '"+y',                                                                                     desc = 'Yank (Clipboard)',                mode = { 'v', 'x' } },
   { map = '<leader>Y',       cmd = '"+Y',                                                                                     desc = 'Yank (Clipboard)',                mode = { 'v', 'x' }, },
   { map = '<leader>p',       cmd = '"+p',                                                                                     desc = 'Paste (Clipboard)',               mode = { 'v', 'x' }, },
   { map = '<leader>P',       cmd = '"+P',                                                                                     desc = 'Paste (Clipboard)',               mode = { 'v', 'x' }, },
